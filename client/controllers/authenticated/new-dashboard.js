a9f21dd0ed9a58fde4f11974a774a21b5b2c1b90
Nick Boucart <nick.boucart@sirris.be>
2015-04-22 11:14:25 +0200

    foutje

--- client/controllers/authenticated/new-dashboard.js
+++ client/controllers/authenticated/new-dashboard.js
@@ -36,12 +36,13 @@ Template.newDashboard.onRendered(function() {
       var keywords = [$('[name="keyword1"]').val(), $('[name="keyword2"]').val(), $('[name="keyword3"]').val()];
       // strip out empty strings if necessary
       keywords = _.compact(keywords);
-      Meteor.call("newDashboard", keywords, function(error, result) {
+      Meteor.call("newDashboard", keywords, function(error, _id) {
         if (error) {
           Bert.alert("Something went wrong:" + error.reason, 'danger');
         }
-        if (result) {
+        if (_id) {
           Bert.alert("Dashboard saved successfully", 'success');
+          Router.go('dashboard.show',{_id: _id});
         }
       });
     }
